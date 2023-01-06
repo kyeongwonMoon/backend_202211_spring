8d845836ba3db13d15eb2240d4a263117727cf85
문경원 <mkw1999@naver.com>
2023-01-06 09:31:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# backend_202211_spring
---
+++ src/main/java/com/kw/board/dto/HelloDto.java
@@ -0,0 +1,14 @@
+package com.kw.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloDto {
+	private String name;
+	private int age;
+	
+}
